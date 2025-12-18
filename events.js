593aed90138e5fddc232bda99a014a91ae2561c9
Vick <vickyao612@gmail.com>
2025-12-18 01:35:35 +0100

    Fix hiding items. Refactor code structure

--- events.js
+++
@@ -1,8 +0,0 @@
-class GameEvent {
-    constructor(type, description, sourceId) {
-        this.type = type; // 'say', 'enter', 'leave', 'action'
-        this.description = description;
-        this.sourceId = sourceId;
-        this.timestamp = Date.now();
-    }
-}
